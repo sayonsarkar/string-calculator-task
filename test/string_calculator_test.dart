6b8f9a154d972c8775ec7c4cedd97229e8634b32
shayonaubie <shayon@auberginesolutions.com>
2025-07-22 22:20:04 +0530

    RED: Added failing test for newline-separated numbers
    Test: add('1\n2,3') should return 6
    Current implementation: only splits by comma, int.parse('1\n2') fails

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -17,4 +17,8 @@ void main() {
   test('multiple numbers separated by comma returns their sum', () {
     expect(add('1,2,3,4,5'), 15);
   });
+
+  test('numbers separated by newlines returns their sum', () {
+    expect(add('1\n2,3'), 6);
+  });
 }
